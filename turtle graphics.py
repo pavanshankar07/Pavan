28861586401ff3d822412b9c52948fb33a3c4612
pavanshankar07 <96132442+pavanshankar07@users.noreply.github.com>
2023-08-19 18:05:16 +0530

    Create turtle graphics.py

---
+++ turtle graphics.py
@@ -0,0 +1,7 @@
+from turtle import *
+for i in range(10,1000,10):
+  forward(i)
+  left(144)
+#simple code you can add speed to the above code by 
+speed() function
+#you can change the directions you want to change left, right,up,down and changing the angles
